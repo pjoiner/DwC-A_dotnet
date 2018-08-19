5a4240a0895630b528b16d7c0b43040a956e5fe6
Paul Joiner <pjoiner@airmail.net>
2018-08-19 11:43:15 -0500

    Added MetaDataReader and some test data

---
+++ src/DWC_A/Meta/MetaDataReader.cs
@@ -0,0 +1,26 @@
+﻿using Dwc.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DwC_A.Meta
+{
+    public class MetaDataReader
+    {
+        private const string MetaFileName = "meta.xml";
+
+        /// <summary>
+        /// Deserializes the meta.xml file into an Archive object
+        /// </summary>
+        /// <param name="path">Path to the meta.xml file (excluding filename)</param>
+        /// <returns>Archive object</returns>
+        public Archive ReadMetaData(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Archive));
+            string fileName = Path.Combine(path, MetaFileName);
+            using (Stream stream = new FileStream(fileName, FileMode.Open))
+            {
+                return serializer.Deserialize(stream) as Archive;
+            }
+        }
+    }
+}
---
+++ src/Tests/MetaDataTests.cs
@@ -0,0 +1,17 @@
+﻿using DwC_A.Meta;
+using Xunit;
+
+namespace Tests
+{
+    public class MetaDataTests
+    {
+
+        [Fact]
+        public void ArchiveShouldContainCoreFile()
+        {
+            var metaDataReader = new MetaDataReader();
+            var archive = metaDataReader.ReadMetaData("./resources/dwca-vascan-v37.5");
+            Assert.NotNull(archive);
+        }
+    }
+}
