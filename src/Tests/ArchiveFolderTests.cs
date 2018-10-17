c0e24f55cfcb34a20ed2347028422d6ca4a28a9b
Paul Joiner <pjoiner@airmail.net>
2018-10-16 19:52:11 -0500

    Modified ArchiveFolder to extract to unique temp paths

---
+++ src/Tests/ArchiveFolderTests.cs
@@ -0,0 +1,22 @@
+﻿using DwC_A;
+using Xunit;
+
+namespace Tests
+{
+    public class ArchiveFolderTests
+    {
+        private const string archiveFileName = "./resources/dwca-vascan-v37.5.zip";
+
+        [Fact]
+        public void ShouldOpenNewInstanceInAnotherDirectory()
+        {
+            var archiveFolder1 = new ArchiveFolder(archiveFileName);
+            var first = archiveFolder1.Extract();
+            var archiveFolder2 = new ArchiveFolder(archiveFileName);
+            var second = archiveFolder2.Extract();
+            archiveFolder1.DeleteFolder();
+            archiveFolder2.DeleteFolder();
+            Assert.NotEqual(first, second);
+        }
+    }
+}
