afbb154e5070b9de50efa7df74e4602c084b5489
Paul Joiner <pjoiner@bwh.harvard.edu>
2022-11-08 16:53:24 -0600

    Updated to net7.0

--- Benchmarks/Config.cs
+++ Benchmarks/Config.cs
@@ -14,6 +14,7 @@ namespace Benchmarks
             AddJob(package_0_7_0.WithRuntime(ClrRuntime.Net48));
             AddJob(package_0_7_0.WithRuntime(CoreRuntime.Core50));
             AddJob(package_0_7_0.WithRuntime(CoreRuntime.Core60));
+            AddJob(package_0_7_0.WithRuntime(CoreRuntime.Core70));
 #else
             var package_0_5_2 = baseJob.WithNuGet("DwC-A_dotnet", "0.5.2").WithId("0.5.2");
             var package_0_6_2 = baseJob.WithNuGet("DwC-A_dotnet", "0.6.2").WithId("0.6.2");
@@ -21,9 +22,11 @@ namespace Benchmarks
             AddJob(package_0_5_2.WithRuntime(ClrRuntime.Net48));
             AddJob(package_0_5_2.WithRuntime(CoreRuntime.Core50));
             AddJob(package_0_5_2.WithRuntime(CoreRuntime.Core60));
+            AddJob(package_0_5_2.WithRuntime(CoreRuntime.Core70));
             AddJob(package_0_6_2.WithRuntime(ClrRuntime.Net48));
             AddJob(package_0_6_2.WithRuntime(CoreRuntime.Core50));
             AddJob(package_0_6_2.WithRuntime(CoreRuntime.Core60));
+            AddJob(package_0_6_2.WithRuntime(CoreRuntime.Core70));
 #endif
         }
     }
